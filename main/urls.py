d7a03b3451d1e25df252e146dce35f8d2e43e559
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-20 12:24:02 +0300

    +++ added urls in app main

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from main import views
+
 app_name = "main"
 
-urlpatterns = []
+urlpatterns = [
+    path("route", views.route, name="route"),
+    path("map", views.map, name="map"),
+]
