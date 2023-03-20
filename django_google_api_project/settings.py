fa8e3b66ff2fab7341968dd8bf4cd9bb8ee496d7
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-20 12:28:29 +0300

    +++ added templates in settings.py

--- django_google_api_project/settings.py
+++ django_google_api_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "django_google_api_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
