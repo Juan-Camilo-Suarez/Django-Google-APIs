0c8ae81f7c8a7b630035abfcbce73aaf0e16b126
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-26 09:55:32 +0300

    +++ added setting to connect with docker bd

--- django_google_api_project/settings.py
+++ django_google_api_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "django_google_api_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME", "django_google_api"),
+        "USER": os.environ.get("DB_USER", "django_google_api"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "django_google_api"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "DATABASE_PORT": "5432",
     }
 }
 
