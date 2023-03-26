dd071bd9a3e99a5a130dc03fe606a9083790a4d9
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-26 09:38:32 +0300

    corrected include namespaces

--- django_google_api_project/urls.py
+++ django_google_api_project/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
+
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django_google_api_project import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls"), namespace="main"),
-    path("", include("users.urls"), namespace="users"),
+    path("", include("main.urls", namespace="main")),
+    path("", include("users.urls", namespace="users")),
 ]
 
 # esta linea hace la funcion que hace un nginx servir archivos estaticos
